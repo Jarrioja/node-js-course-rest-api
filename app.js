7c9f6a95a4abbe38c3df1bb61f7759a899dbeff3
Jesus Arrioja <Jarrioja2210@gmail.com>
2022-12-21 16:05:53 -0300

    445. Fixing a Pagination Bug

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const { graphqlHTTP } = require("express-graphql");
 
 const graphqlSchema = require("./graphql/schema");
 const graphqlResolver = require("./graphql/resolvers");
+const auth = require("./middleware/auth");
+const { clearImage } = require("./util/file");
 
 const MONGODB_URI =
   "mongodb+srv://jesus:12345@cluster0.jbpnnov.mongodb.net/messages";
@@ -51,6 +53,22 @@ app.use((req, res, next) => {
     return res.sendStatus(200);
   }
   next();
+});
+app.use(auth);
+
+app.put("/post-image", (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error("Not authenticated");
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: "No file provided" });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+    .status(201)
+    .json({ message: "File stored", filePath: req.file.path });
 });
 
 app.use(
--- graphql/schema.js
+++ graphql/schema.js
@@ -4,8 +4,8 @@ module.exports = buildSchema(`
     type Post {
         _id: ID!
         title: String!
-        content: String
-        imageUrl:String!
+        content: String!
+        imageUrl: String!
         creator: User!
         createdAt: String!
         updatedAt: String!
@@ -14,26 +14,47 @@ module.exports = buildSchema(`
     type User {
         _id: ID!
         name: String!
-        email: String
-        status:String!
-        posts:[Post!]
+        email: String!
+        password: String
+        status: String!
+        posts: [Post!]!
     }
 
     type AuthData {
-        token:String!
-        userId:String!
+        token: String!
+        userId: String!
     }
+
+    type PostData {
+        posts: [Post!]!
+        totalPosts: Int!
+    }
+
     input UserInputData {
         email: String!
         name: String!
         password: String!
     }
-    type RootQuery {
-        login(email: String!, password: String!): AuthData!
+
+    input PostInputData {
+        title: String!
+        content: String!
+        imageUrl: String!
     }
 
-    type RootMutation{
+    type RootQuery {
+        login(email: String!, password: String!): AuthData!
+        posts(page: Int): PostData!
+        post(id: ID!): Post!
+        user: User!
+    }
+
+    type RootMutation {
         createUser(userInput: UserInputData): User!
+        createPost(postInput: PostInputData): Post!
+        updatePost(id: ID!, postInput: PostInputData): Post!
+        deletePost(id: ID!): Boolean
+        updateStatus(status: String!): User!
     }
 
     schema {
